274037fe25b048b3422634a843f0d7ed1073ccce
oleksiikolii <igravkompik@gmail.com>
2023-08-14 21:22:52 +0300

    implemented tg bot activation check to borrow books

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from borrowing.models import Borrowing
-from borrowing.serializers import (
-    BorrowingSerializer,
-    BorrowingDetailSerializer
-)
+from borrowing.permissions import TGBotActivated
+from borrowing.serializers import BorrowingSerializer, BorrowingDetailSerializer
 
 
 class BorrowingViewSet(ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+    permission_classes = [IsAuthenticated, TGBotActivated]
 
     def get_serializer_class(self):
         if self.action == "list":
