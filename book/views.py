f1b6a7f28072f7377a405b023800c6dacc229b6a
user <volodimirkomarnickij28@gmail.com>
2023-08-14 15:03:11 +0300

    Create view for book model and add paths to reach endpoint

--- book/views.py
+++ book/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from book.permissions import IsAdminOrReadOnly
+from book.models import Book
+from book.serializers import BookSerializer
+
+
+class BookViewSet(
+    viewsets.ModelViewSet
+):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = (IsAdminOrReadOnly, )
