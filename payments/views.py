e562b866234b17c82014122a0d057125a11fe145
Anton Komarov <anton_komarov_qa@ukr.net>
2023-08-14 20:02:41 +0300

    Add Payment List and Detail Views

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+from borrowing.permissions import IsOwnerOrAdmin
+from payments.models import Payment
+from payments.serializers import PaymentSerializer
+
+
+class PaymentListView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Payment.objects.all()
+        else:
+            return Payment.objects.filter(user=user)
+
+
+class PaymentDetailView(generics.RetrieveAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrAdmin]
