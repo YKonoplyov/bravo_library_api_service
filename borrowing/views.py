d86044d061e911fd9896dce2fbdfa888ba28b853
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-14 18:22:59 +0300

    added logic for retrieving different serializers for different actions

--- borrowing/views.py
+++ borrowing/views.py
@@ -10,4 +10,11 @@ from borrowing.serializers import (
 
 class BorrowingViewSet(ModelViewSet):
     queryset = Borrowing.objects.all()
-    serializer_class = BorrowingDetailSerializer
+    serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingSerializer
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+        return BorrowingSerializer
