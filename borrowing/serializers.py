37d3da51614ee67f60ea18441660cedc5ffee630
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-14 20:35:12 +0300

    added validation to Borrowing serializer that prevent creating borrow if there is no books in inventory

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,16 +1,24 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
+from book.models import Book
 from book.serializers import BookSerializer
 from borrowing.models import Borrowing
 from user.serializers import UserSerializer
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        data = super(BorrowingSerializer, self).validate(attrs=attrs)
+        book_id = attrs.get("book_id").id
+        book = Book.objects.get(id=book_id)
+        if book.inventory == 0:
+            raise ValidationError(f"There is no copies of {book.title}")
+        return data
 
     class Meta:
         model = Borrowing
         fields = ("id", "borrow_date", "expected_return_date", "book_id")
-
 
 
 class BorrowingDetailSerializer(BorrowingSerializer):
