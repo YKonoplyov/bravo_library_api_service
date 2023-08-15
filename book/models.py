650c3ad11f92e602a5ed63694d53f7a6d8a118fa
user <volodimirkomarnickij28@gmail.com>
2023-08-15 14:35:03 +0300

    Change path name

--- book/models.py
+++ book/models.py
@@ -17,7 +17,7 @@ def book_image_file_path(instance, filename):
 
     filename = f"{slugify(instance.title)}-{uuid.uuid4()}.{extension}"
 
-    return os.path.join("uploads/movies/", filename)
+    return os.path.join("uploads/books/", filename)
 
 
 class Book(models.Model):
