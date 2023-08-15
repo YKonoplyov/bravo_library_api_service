66c6440210239b005d6e2452619badc233937b34
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-15 11:24:30 +0300

    installed django-q

--- library_config/settings.py
+++ library_config/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
+
+    "django_q"
     "rest_framework",
     "debug_toolbar",
     
