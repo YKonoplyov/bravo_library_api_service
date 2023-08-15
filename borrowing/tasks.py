a59cdb10c371dc238dc108938e3c1fc30d7dfb7a
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-15 14:22:45 +0300

    deleted unnecessary docstring, fixed filtering of borrowings

--- borrowing/tasks.py
+++ borrowing/tasks.py
@@ -7,8 +7,6 @@ from services.notification.bot_manager import TelegramBot
 def get_overdue_borrowings():
     today_date = timezone.datetime.now().date()
     overdue_borrowings = Borrowing.objects.filter(
-        actual_return_date__lt=today_date
+        actual_return_date__isnull=True, expected_return_date__lt=today_date
     )
-    TelegramBot.send_message(overdue_borrowings)
-    """uncomment to activate message sending 
-    for users fir overdued borrowings"""
+    TelegramBot.send_notifications(overdue_borrowings)
