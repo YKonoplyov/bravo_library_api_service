4bdd3c9ba5ac076f2859f4c1a390715784440402
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-15 17:31:53 +0300

    final commit

--- borrowing/tasks.py
+++ borrowing/tasks.py
@@ -2,7 +2,7 @@ from django.db.models import Q
 from django.utils import timezone
 
 from borrowing.models import Borrowing
-# from services.notification.bot_manager import TelegramBot
+from services.notification.bot_manager import TelegramBot
 
 
 def get_overdue_borrowings():
@@ -10,7 +10,7 @@ def get_overdue_borrowings():
     active_borrowings = Borrowing.objects.filter(
         actual_return_date__isnull=True
     )
-    overdue_borrowings = Borrowing.objects.filter(
+    overdue_borrowings = active_borrowings.filter(
             expected_return_date__lt=today_date
         )
-    print(overdue_borrowings)
+    TelegramBot.send_notification(overdue_borrowings)
