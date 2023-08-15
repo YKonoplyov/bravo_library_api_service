dbfac15992adac46e287859469675ee702fbcd87
Anton Komarov <anton_komarov_qa@ukr.net>
2023-08-15 13:51:44 +0300

    Add blank=True in session_url and user_id

--- payments/models.py
+++ payments/models.py
@@ -26,8 +26,8 @@ class Payment(models.Model):
         Borrowing,
         on_delete=models.CASCADE
     )
-    session_url = models.URLField()
-    session_id = models.CharField(max_length=255)
+    session_url = models.URLField(blank=True)
+    session_id = models.CharField(max_length=255, blank=True)
     money_to_pay = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
