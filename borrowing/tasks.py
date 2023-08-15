bba8489bcb926b5065b2898ebc546b5322b98c48
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-15 14:25:17 +0300

    added select_realted for tables user and book

--- borrowing/tasks.py
+++ borrowing/tasks.py
@@ -8,5 +8,5 @@ def get_overdue_borrowings():
     today_date = timezone.datetime.now().date()
     overdue_borrowings = Borrowing.objects.filter(
         actual_return_date__isnull=True, expected_return_date__lt=today_date
-    )
+    ).select_related("user_id", "book_id")
     TelegramBot.send_notifications(overdue_borrowings)
